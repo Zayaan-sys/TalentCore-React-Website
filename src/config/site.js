11643760b8904b2193476ff9beba5b04e84c1637
Zayaan-sys <130749140+Zayaan-sys@users.noreply.github.com>
2026-04-29 13:50:22 +0200

    added tech number

--- src/config/site.js
+++ src/config/site.js
@@ -1,7 +1,7 @@
 export const FORM_ENDPOINT = "https://formspree.io/f/mvzdenlb";
 export const CAL_BOOKING_URL = "https://cal.com/zayaan-lodewyk-pcaqyn";
-export const PSYCHOLOGY_WHATSAPP_NUMBER = "27726044024";
-export const TECHNOLOGY_WHATSAPP_NUMBER = "";
+export const PSYCHOLOGY_WHATSAPP_NUMBER = "0823030059";
+export const TECHNOLOGY_WHATSAPP_NUMBER = "0833693711";
 
 export const siteLinks = {
   formEndpoint: FORM_ENDPOINT,
